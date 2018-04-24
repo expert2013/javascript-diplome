39bb53906fad5fd7a49153f6b3543517060a2b65
Expert <expert2006@ukr.net>
2018-04-24 08:49:59 +0300

    Diplome work part2

--- js/script.js
+++ js/script.js
@@ -19,6 +19,11 @@ let popupBtn = document.getElementById('popup-btn'),
       inputName = customInfo.getElementsByTagName('input')[0],
       mainButtons = document.getElementsByClassName('main-buttons')[0],
       crimeBtn = document.getElementById('crime')
+      //photos = document.getElementsByClassName('photo')
+     
+
+    // console.log(label);
+    
 
     customInfo.insertBefore(label, inputName);
     mainButtons.appendChild(crimeBtn);
@@ -30,6 +35,7 @@ let popupBtn = document.getElementById('popup-btn'),
         customInfo.style.display = "block";
         customChar.style.display = "block";
         customStyle.style.display = "block";  
+
 });
 
 // hide image
@@ -53,6 +59,7 @@ function defaultSlide(index, typeStyle){
      
    } 
 }
+
 // В разработке!!! 
 // let customInput = document.querySelectorAll('.custom-info input');  
 // function clearInput(input) {
@@ -65,13 +72,16 @@ function defaultSlide(index, typeStyle){
       personClothes = personConstruct.children[1],
       personHair = personConstruct.children[2],
       personShoes = personConstruct.children[3]
-    
 
  let imgSkins = 'background: url("img/skin/skin-1.png") center no-repeat; background-size: cover',
      imgClothes = 'background: url("img/clothes/construct/clothes-1.png") center no-repeat; background-size: cover',
      imgHairs = 'background: url("img/hair/construct/hair-1.png") center no-repeat; background-size: cover',
      imgShoes =' background: url("img/clothes/construct/shoes-1.png") center no-repeat; background-size: cover'
     
+  personSkin.style.cssText = getBackGround(imgSkins , 1); // slideIndex
+  personClothes.style.cssText = getBackGround(imgClothes , 1);
+  personHair.style.cssText = getBackGround(imgHairs , 1);
+  personShoes.style.cssText = getBackGround(imgShoes , 1);
 
 function getBackGround(str, index){
   let num = parseInt(str.replace(/\D+/g,""));
@@ -81,10 +91,17 @@ function getBackGround(str, index){
   
 }; 
 
+ let skinColor = document.getElementsByClassName('skin-color'), 
+    hairStyle = document.getElementsByClassName('hair-style'),
+    clothesStyle = document.getElementsByClassName('clothes-style') 
+
+
+
 // Получение данных из первой формы  
 
 let readyBtn = document.getElementById('ready')
   readyBtn.addEventListener('click', function() {
+  
       let person = {
           fio: inputName.value,
           age: inputAge.value,
@@ -100,6 +117,29 @@ let readyBtn = document.getElementById('ready')
     mainCardsNew.style.border = "2px solid green"; //временный стиль
     document.body.appendChild(mainCardsNew);
     mainCardsItem[0].parentNode.insertBefore(mainCardsNew, mainCardsItem[1].nextSibling);
+
+ let photo = mainCardsNew.childNodes[1].childNodes[1];
+     mainCardsNew.childNodes[1].insertBefore(personConstruct,photo);
+     //personConstruct.style.border = "1px solid green";
+     photo.parentNode.removeChild(photo);
+
+
+ let btnReset = document.getElementById('reset');
+
+ btnReset.addEventListener('click', function(){
+  alert('hello');
+});   
+
+
+   //let candidateBlock = document.getElementsByClassName('candidate-block');
+  //console.log(mainCardsNew.childNodes[1].childNodes[1]);       
+
+
+        // if (getGender() == 'Женский') {
+             // candidateBlock[3].insertBefore(personConstruct[0],photos[1]);
+       //  }  else {
+       //        candidateBlock[2].insertBefore(personConstruct[0],photos[1]);
+       //  };
     
     mainCardsNew.children[1].textContent = person.fio;
     mainCardsNew.children[2].textContent = person.age;
@@ -115,9 +155,7 @@ let readyBtn = document.getElementById('ready')
  radioBtn.addEventListener('click', function () {
  
 
-let skinColor = document.getElementsByClassName('skin-color'), 
-    hairStyle = document.getElementsByClassName('hair-style'),
-    clothesStyle = document.getElementsByClassName('clothes-style') 
+
 
      if (getGender() == 'Женский') {
     
@@ -128,7 +166,7 @@ let skinColor = document.getElementsByClassName('skin-color'),
        defaultSlide(1, skinColor);
        defaultSlide(4, hairStyle);
        defaultSlide(4, clothesStyle);
-       clearInput();
+       //clearInput();
       indexGender = 1; 
   } else {
       personSkin.style.cssText = getBackGround(imgSkins , 1); // slideIndex
@@ -138,7 +176,7 @@ let skinColor = document.getElementsByClassName('skin-color'),
        defaultSlide(0, skinColor);
        defaultSlide(0, hairStyle);
        defaultSlide(0, clothesStyle);
-       clearInput();
+       //clearInput();
       indexGender = 0;
   }; 
 
